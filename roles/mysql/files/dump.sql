3ae887c0a6380853d248271128f4df6364c0de46
Neha-dot-Yadav <80828738+Neha-dot-Yadav@users.noreply.github.com>
2021-05-28 13:16:15 +0530

    Add files via upload

---
+++ roles/mysql/files/dump.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS user (
+  idol_id int PRIMARY KEY,
+  idol_name varchar(20) NOT NULL,
+  details varchar(200) NOT NULL         
+  );
+  INSERT IGNORE INTO user VALUES(1,'Jin','World wide handsome');
+  INSERT IGNORE INTO user VALUES(2,'Suga','Savage');
+  INSERT IGNORE INTO user VALUES(3,'JHope','Sunshine');
+  INSERT IGNORE INTO user VALUES(4,'RM','God of destruction');
+  INSERT IGNORE INTO user VALUES(5,'Jimin','Sweet');        
+  INSERT IGNORE INTO user VALUES(6,'Tae','Adult child');
+  INSERT IGNORE INTO user VALUES(7,'JK','Golden maknae');
